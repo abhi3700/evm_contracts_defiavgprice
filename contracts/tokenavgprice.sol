9760404daf25d3930d6e2558cbb2c5feb4745ee4
Abhijit Roy <dalvath3700@gmail.com>
2021-12-27 22:38:50 +0530

    token avg price name updated

--- contracts/tokenavgprice.sol
+++ contracts/tokenavgprice.sol
@@ -8,8 +8,8 @@ import '@openzeppelin/contracts/utils/Context.sol';
 import "hardhat/console.sol";
 
 /// @notice A keeper pushes a price of token
-/// @dev Find out the avg price of token for a time interval without iterating in for-loop
-contract Strips is Ownable, Pausable {
+/// @dev Find out the avg price of token for a time interval without iterating in loops (for, while)
+contract tokenavgprice is Ownable, Pausable {
 
     using SafeMath for uint256;
 
